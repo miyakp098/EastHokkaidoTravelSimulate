15539367217a1587c0620cc1098e82fc9514adf6
miyakp098 <ybtkeppen813@gmail.com>
2024-01-14 17:18:59 +0900

    index.ejsに変更、API読み込み

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;  // Render が使用するポート、またはローカルで3000ポートを使用
+const port = process.env.PORT || 3000;
+
+// EJSビューエンジンを設定
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // 静的ファイルを提供するためのディレクトリを設定
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// ルートURLへのリクエストに対してindex.htmlを提供
+// ルートURLへのリクエストに対してindex.ejsを提供（Google Maps APIキーを含む）
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.render('index', { apiKey: process.env.GOOGLE_MAPS_API_KEY });
 });
 
 // サーバーを起動
